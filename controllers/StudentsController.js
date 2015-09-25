820b74bc83a437ab3662fea308f053a695b23346
CharlesAMoss <charlesamoss@yahoo.com>
2015-09-25 16:47:11 -0700

    add AttendanceController

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,4 +1,14 @@
-studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory) {
+signIn.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory) {
     $scope.students = StudentsFactory.students;
     $scope.StudentsFactory = StudentsFactory;
+
+    $scope.signInStudent = function(student) {
+        student.isHere = true;
+    };
+
+    $scope.signOutStudent = function(student) {
+        student.isHere = false;
+    };
+
+
 });
--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,32 +1,38 @@
-studentRoster.factory('StudentsFactory', function StudentsFactory() {
+signIn.factory('StudentsFactory', function StudentsFactory() {
     var factory = {};
     factory.students = [{
         name: "Juan Epstein",
+        lastName: "Epstein",
         image: "/image/epstein.jpg",
         isHere:false
     }, {
         name: "Freddie Washington",
+        lastName: "Washington",
         image: "/image/washington.jpg",
         isHere:false
     }, {
         name: "Vincent Barbarino",
+        lastName: "Barbarino",
         image: "/image/barbarino.jpg",
         isHere:false
     }, {
         name: "Arnold Horshack",
+        lastName: "Horshack",
         image: "/image/horshack.jpg",
         isHere:false
     }];
 
-    factory.addStudent = function() {
-        var student = { name:factory.studentName, inClass:false };
-        factory.students.push(student);
-        factory.studentName = null;
-    };
 
-    factory.deleteStudent = function(student) {
-        var index = factory.students.indexOf(student);
-        factory.students.splice(index, 1);
-    };
+
+    // factory.addStudent = function() {
+    //     var student = { name:factory.studentName, isHere:false };
+    //     factory.students.push(student);
+    //     factory.studentName = null;
+    // };
+    //
+    // factory.deleteStudent = function(student) {
+    //     var index = factory.students.indexOf(student);
+    //     factory.students.splice(index, 1);
+    // };
     return factory;
 });
